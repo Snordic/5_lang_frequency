5a02a694c9da4ffccbf81ba0db3add504237a0f7
Snordic <artstr16@gmail.com>
2018-03-23 23:18:52 +0500

    Переписал функций

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,14 +4,8 @@ import sys
 
 
 def load_data(filepath):
-    try:
-        with open(filepath, 'r', encoding='utf-8') as file_with_txt:
-            data_form_file = file_with_txt.read()
-    except FileNotFoundError:
-        print('Ошибка: Данный файл не существует!')
-        return None
-    else:
-        return data_form_file.lower()
+    with open(filepath, 'r', encoding='utf-8') as file_with_txt:
+        return file_with_txt.read()
 
 
 def clear_characters(text):
@@ -25,22 +19,24 @@ def get_most_frequent_words(data_for_search, number_words=10):
 
 
 def print_word_repetitons(counted_words):
-    for key, value in counted_words:
-        print('{} => {}'.format(key, value))
+    for word, count in counted_words:
+        print('{} => {}'.format(word, count))
 
 
-def main(filepath):
-    data_from_file = load_data(filepath)
-    if data_from_file:
-        list_words = clear_characters(data_from_file)
-        counted_words = get_most_frequent_words(list_words)
-        print_word_repetitons(counted_words)
+def format_lower(text):
+    return text.lower()
 
 
 if __name__ == '__main__':
     try:
         filename = sys.argv[1]
+        data_from_file = load_data(filename)
     except IndexError:
         print('Ошибка: Не добавлен файл для поиска!')
+    except FileNotFoundError:
+        print('Ошибка: Данный файл не существует!')
     else:
-        main(filename)
+        text_lower = format_lower(data_from_file)
+        list_words = clear_characters(text_lower)
+        counted_words = get_most_frequent_words(list_words)
+        print_word_repetitons(counted_words)
